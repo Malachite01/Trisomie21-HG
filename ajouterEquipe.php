a3af9d82b7e87d67fcdf28f0b30d1cb6232aa883
Paul Thouverey <paul.thouverey@etu.iut-tlse3.fr>
2022-12-05 11:12:30 +0100

    AjouterEquipeFonctionne

--- ajouterEquipe.php
+++ ajouterEquipe.php
@@ -9,30 +9,37 @@ require('QUERY.php');
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="icon" type="image/x-icon" href="images/favicon.png">
+  <link rel="shortcut icon" type="image/x-icon" href="images/favicon.png">
+  <link rel="stylesheet" href="style/style.css">
 </head>
 <body>
-<?php afficherNomPrenomEnfant();?>
+    <form action="" method="POST">
+        
+        
+<?php 
+faireMenu();
+afficherNomPrenomEnfant();?>
 <?php afficherNomPrenomMembre();?>
 <label for="champRole">Role :</label>
-      <input type="text" name="champRole" placeholder="Entrer le role de cette personne" minlength="1" maxlength="50" required>
+      <input type="text" name="champRole" placeholder="Entrer le rôle de cette personne" minlength="1" maxlength="50" required>
       <span></span>
 
       <div class="center" id="boutonsValiderAnnuler">
       <button type="reset" name="boutonAnnuler" class="boutonAnnuler"><img src="images/annuler.png" class="imageIcone" alt="icone annuler"><span>Annuler</span></button>
-      <button type="submit" name="boutonValider" class="boutonValider"><img src="images/valider.png" class="imageIcone" alt="icone valider"><span>Valider</span></button>
+      <button type="submit" name="boutonValider" class="boutonValider" id="boutonValider"><img src="images/valider.png" class="imageIcone" alt="icone valider"><span>Valider</span></button>
     </div>
     <?php
         
-    if (champRempli(array('ChampRole'))){
+    if (champRempli(array('champRole'))){
            AjouterUneEquipe(
             $_POST['idEnfant'],
             $_POST['idMembre'],
-            mktime(),
+            time(),
             $_POST['champRole']
-        )
+           );
     }
     ?>
-    
-    
+    </form>
 </body>
 </html>
