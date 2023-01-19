a379fa828af83235358e8f70df4a64409e2f93c1
Paul Thouverey <paul.thouverey@etu.iut-tlse3.fr>
2023-01-19 10:38:13 +0100

    Paul Coucou

--- consulterObjectif.php
+++ consulterObjectif.php
@@ -75,7 +75,7 @@ faireMenu();
             }
         }
         // 2419200 = 4 weeks
-        if (recupererPremierJetonJamaisPose($_SESSION['objectif']) == null ||recupererPremierJetonJamaisPose($_SESSION['objectif']) + 2419200  >= time()) {
+        if (recupererPremierJetonJamaisPose($_SESSION['objectif']) == null ||recupererPremierJetonJamaisPose($_SESSION['objectif']) + 20  >= time()) {
             afficherObjectifsZoom($_SESSION['objectif']);
            ?>
         <div id="containerRecompenses">
@@ -87,11 +87,14 @@ faireMenu();
        <?php
        afficherBarresProgression($_SESSION['objectif']);
         } else {
-            echo '<h1> Objectif terminé !';
+            echo '<h1> Objectif terminé !';?>
+
+            <button type="button" id="boutonStats" class="boutonEdit" onclick="createPieChart('chartData','chartTata'),fenOpenStats('containerStats'),deCache('containerStats');"><img src="images/flecheBas.png" id="flecheBas"><span></span></button>
+            <?php
             afficherBarresProgression($_SESSION['objectif']);
             if ($_SESSION['dataTruc'] >= 80){
                echo '<button type="submit" name="boutonAVenir" class="boutonValider" id="boutonValider"><img src="images/valider.png" class="imageIcone" alt="icone valider"><span>Valider</span></button>';
-              // echo '<button type="submit" name="butonDebutSeance" class="boutonValider boutonSeance"><img src="images/valider.png" class="imageIcone" alt="icone valider"><span>Démarrer la séance</span></button>';
+              //echo '<button type="submit" name="butonDebutSeance" class="boutonValider boutonSeance"><img src="images/valider.png" class="imageIcone" alt="icone valider"><span>Démarrer la séance</span></button>';
 
                nettoyerObjectif();
 
