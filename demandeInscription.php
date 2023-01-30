3441821339ee689b634182d55ec67be9c2752b26
Mathieu Antunes <mathieu.antunes31@gmail.com>
2023-01-30 08:54:42 +0100

    Mathieu zoom smartphone

--- demandeInscription.php
+++ demandeInscription.php
@@ -4,7 +4,7 @@
 <head>
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta name="description" content="">
   <title>Demande inscription</title>
   <link rel="icon" type="image/x-icon" href="images/favicon.png">
